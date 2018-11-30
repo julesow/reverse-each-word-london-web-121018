6ca0cf3b0150077aca5ec5bd1fe94572c9abb52f
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-30 06:37:33 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+def reverse_each_word(string) 
+  new_array=[]
+  string.split(" ").each do|string|
+  new_array << string.reverse!
+  end
+return new_array.join(" ")
+end
+reverse_each_word("Hello there, and how are you?")
+
+
+
+def reverse_each_word(string) 
+  new_array=[]
+  string.split(" ").collect do|string|
+  new_array << string.reverse!
+  end
+return new_array.join(" ")
+end
+reverse_each_word("Hello there, and how are you?")
